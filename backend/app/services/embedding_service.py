0c33ddea0be859d10f379fa3d7aa3e5a7659735c
Hirak Jyoti Chakraborty <hirakchakraborty99@gmail.com>
2026-08-29 13:00:30 +0530

    did few more changes

--- backend/app/services/embedding_service.py
+++ backend/app/services/embedding_service.py
@@ -5,14 +5,15 @@ class EmbeddingService:
 
     def __init__(self):
         self.model = SentenceTransformer(
-            "sentence-transformers/all-MiniLM-L6-v2"
+            "BAAI/bge-small-en-v1.5"
         )
 
     def generate_embeddings(self, chunks: list[str]):
 
         embeddings = self.model.encode(
             chunks,
-            convert_to_numpy=True
+            convert_to_numpy=True,
+            normalize_embeddings=True
         )
 
         return embeddings
